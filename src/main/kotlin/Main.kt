1c96a771e84423e9f9a1a83afca7a1efe4ebf0c0
Felipe Mattedi <felipe.mattedi@terra.com.br>
2022-05-02 23:45:21 -0300

    Primeiras mensagens

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,9 @@
 fun main() {
-    println("Hello World!")
+    val usuario = "Felipe"
+    val cod_usuario : String = "100001"
+    val reservas : Int = 0
+    val saudacao = "Bem vindo, $usuario, ao sistema de reservas AeroBook!"
+    val saudacao_aviso = "Voce possui $reservas reservas confirmadas no sistema."
+    println(saudacao)
+    println(saudacao_aviso)
 }
